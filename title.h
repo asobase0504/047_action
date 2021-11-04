8f16a24a43a1c9d472d038df8f1cb2de75e59721
YudaKaito <student@S-JOBI21-029>
2021-11-04 10:48:35 +0900

    tutorial.cppの作成。 title.hの不必要な宣言の削除

--- title.h
+++ title.h
@@ -3,18 +3,6 @@
 
 #include "main.h"
 
-// プレイヤー構造体の定義
-typedef struct
-{
-	D3DXVECTOR3 pos;	// 位置
-	D3DXVECTOR3 rot;	// 向き
-	float fWidth;		// 画像の幅
-	float fHeigth;		// 画像の高さ
-	float fLength;		// 対角線の長さ
-	float fAngle;		// 対角線の角度
-
-}TITLE;
-
 // プロトタイプ宣言
 void InitTitle(void);
 void UninitTitle(void);
